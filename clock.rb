e62e61d040d29dcd46fa294b62a3a2f7b007673f
Garen Torikian <gjtorikian@gmail.com>
2015-04-23 09:02:57 -0700

    Add initial tweets

--- clock.rb
+++ clock.rb
@@ -1,7 +1,16 @@
 require 'rubygems'
 require 'clockwork'
+require 'twitter'
 include Clockwork
+
+# client = Twitter::REST::Client.new do |config|
+#   config.consumer_key        = ENV["42415_TWITTER_CONSUMER_KEY"]
+#   config.consumer_secret     = ENV["42415_TWITTER_CONSUMER_SECRET"]
+#   config.access_token        = ENV["42415_TWITTER_ACCESS_TOKEN"]
+#   config.access_token_secret = ENV["42415_TWITTER_ACCESS_SECRET"]
+# end
+
 handler do |job|
   puts 'Hi there.'
 end
-every(1.day, 'job_name')
+every(5.minutes, 'job_name')
