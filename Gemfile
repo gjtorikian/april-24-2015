920d982266d07d3f84c0441cb65a7620bc09c757
Garen Torikian <gjtorikian@gmail.com>
2015-04-23 17:18:00 -0700

    Post updates with Twitter

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'clockwork', '~> 1.0'
 gem 'twitter', '~> 5.0'
+gem 'dotenv', '~> 2.0'
--- clock.rb
+++ clock.rb
@@ -1,16 +1,25 @@
 require 'rubygems'
 require 'clockwork'
+require 'json'
 require 'twitter'
 include Clockwork
 
-# client = Twitter::REST::Client.new do |config|
-#   config.consumer_key        = ENV["42415_TWITTER_CONSUMER_KEY"]
-#   config.consumer_secret     = ENV["42415_TWITTER_CONSUMER_SECRET"]
-#   config.access_token        = ENV["42415_TWITTER_ACCESS_TOKEN"]
-#   config.access_token_secret = ENV["42415_TWITTER_ACCESS_SECRET"]
-# end
-
-handler do |job|
-  puts 'Hi there.'
+unless ENV['RACK_ENV'] == 'production'
+  require 'dotenv'
+  Dotenv.load
 end
-every(5.minutes, 'job_name')
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV["42415_TWITTER_CONSUMER_KEY"]
+  config.consumer_secret     = ENV["42415_TWITTER_CONSUMER_SECRET"]
+  config.access_token        = ENV["42415_TWITTER_ACCESS_TOKEN"]
+  config.access_token_secret = ENV["42415_TWITTER_ACCESS_SECRET"]
+end
+
+tweets = ['Hey there', 'Tweet out!', 'Quack quack.']
+handler do |_|
+  tweet = tweets.shift
+  client.update(tweet)
+end
+
+every(30.seconds, 'tweet')
