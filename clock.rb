21965b8a6f1a608f4cd2a58321a647e848c8fa0a
Garen Torikian <gjtorikian@gmail.com>
2015-04-23 17:51:28 -0700

    Finish up the tweeting script

--- clock.rb
+++ clock.rb
@@ -1,6 +1,5 @@
 require 'rubygems'
 require 'clockwork'
-require 'json'
 require 'twitter'
 include Clockwork
 
@@ -10,16 +9,23 @@ unless ENV['RACK_ENV'] == 'production'
 end
 
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV["42415_TWITTER_CONSUMER_KEY"]
-  config.consumer_secret     = ENV["42415_TWITTER_CONSUMER_SECRET"]
-  config.access_token        = ENV["42415_TWITTER_ACCESS_TOKEN"]
-  config.access_token_secret = ENV["42415_TWITTER_ACCESS_SECRET"]
+  config.consumer_key        = ENV['42415_TWITTER_CONSUMER_KEY']
+  config.consumer_secret     = ENV['42415_TWITTER_CONSUMER_SECRET']
+  config.access_token        = ENV['42415_TWITTER_ACCESS_TOKEN']
+  config.access_token_secret = ENV['42415_TWITTER_ACCESS_SECRET']
 end
 
-tweets = ['Hey there', 'Tweet out!', 'Quack quack.']
+tweets = ['Gonna start', 'Start two', 'And three?', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13!', '14,', '15']
+
+response = nil
 handler do |_|
+  return if tweets.empty?
   tweet = tweets.shift
-  client.update(tweet)
+  if !response.nil?
+    response = client.update("@gjtorikian #{tweet}", :in_reply_to_status_id => response.id)
+  else
+    response = client.update(tweet)
+  end
 end
 
-every(30.seconds, 'tweet')
+every(2.minutes, 'tweet')
